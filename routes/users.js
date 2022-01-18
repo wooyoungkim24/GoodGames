3e7631d99f8b5e80c82d977cfbba9dd41ff773ee
pierikm <pierikm@umich.edu>
2022-01-18 18:08:42 -0500

    signup and login routes and pugs started

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', (req, res, next) => {
+  res.render('log-in');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('sign-up');
+})
+
 module.exports = router;
