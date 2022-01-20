94530560964f990ba61462b2a51764910c192faa
Benjamin Danishevsky <ben.danishevsky@gmail.com>
2022-01-20 15:36:20 -0500

    added IDs to collections pug template

--- routes/collections.js
+++ routes/collections.js
@@ -11,7 +11,11 @@ router.get('/', asyncHandler(async(req,res) =>{
     const collections = await db.Collection.findAll( { where: { userId: req.session.auth.userId, name: 'Favorites'} });
     // copy line 11 and change to each collection title eg. 'Favorites'
 
-    const userCollected = await db.Collected.findAll( { where: {collectionsId: collections[0].id}, include: 'Game' } );
+    const userCollected = await db.Collected.findAll(
+        {
+            where: { collectionsId: collections[0].id },
+            include: 'Game',
+        } );
     res.render('collections-list', {
         collections,
         userCollected,
