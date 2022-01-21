6b5679f5a197deaa28e391f08321a06804ee4f62
Wooyoung Kim <wooyoungkim24@gmail.com>
2022-01-21 17:02:55 -0500

    finished api

--- routes/games.js
+++ routes/games.js
@@ -25,41 +25,59 @@ router.get('/games', asyncHandler(async(req,res) =>{
     })
 }))
 
-router.get('/games/:id(\\d+)', asyncHandler(async(req,res) =>{
-    let email = '';
-    if(req.session.auth){
-        email = await getUserEmail(req.session.auth.userId);
-    }
+// router.get('/games/api',asyncHandler(async(req, res,next) =>{
+//     const collecteds = await db.Collected.findAll()
+//     const collections = await db.Collection.findAll()
+//     const userId = req.session.auth.userId
+//     // console.log(req)
+//     res.json({collecteds, userId, collections})
+// }))
 
-    const gameId = parseInt(req.params.id, 10);
-    const game = await db.Game.findByPk(gameId)
-    const reviews = await db.Review.findAll({where:{gameId:gameId}, include:"User"})
-    const collectionItems = await db.Collection.findAll();
+
+router.get('/games/:id(\\d+)', asyncHandler(async (req, res) => {
+    let email = '';
+
+
+    let gameId = parseInt(req.params.id, 10);
+    let game = await db.Game.findByPk(gameId)
+    let reviews = await db.Review.findAll({ where: { gameId: gameId }, include: "User" })
+    let collectionItems = await db.Collection.findAll();
+    const userCollecteds = await db.Collected.findAll({where:{gameId:gameId}, include: "Collection"});
+    const userCollectedsCollections = userCollecteds.map(ele => ele.Collection.id)
+
+    let collectionId = 0;
+    let userId = -5;
+    if (req.session.auth) {
+        let test = false;
+        email = await getUserEmail(req.session.auth.userId);
+        userId = req.session.auth.userId
+        let i = 0;
+        if(userCollecteds.length >0){
+            while(test === false){
+
+                let finder= await db.Collection.findByPk(userCollectedsCollections[i]);
+                if (finder.userId === req.session.auth.userId){
+                    collectionId = finder.id;
+                    test = true;
+                }else{
+                    i++;
+                }
+            }
+        }
+    }
 
     res.render('game-single', {
         email,
         game,
         reviews,
+        userCollecteds,
         collectionItems,
+        collectionId,
+        userId,
         body: {},
-        collections: ['Have Played', 'Playing', 'Want to Play']
+        collections: ['Have Played', 'Currently Playing', 'Want to Play']
     })
 }))
-
-// router.get('/games/add', requireAuth, csrfProtection, (req, res) => {
-//     const game = db.Games.build();
-//     res.render('game-add', {
-
-//     })
-// })
-// router.post('/games/add', requireAuth, csrfProtection, gameAddValidators,
-//     asyncHandler(async (req, res) => {
-//         const {collectionsId, gameId} = req.body;
-//         const collection = db.Collection.findByPk(collectionsId)
-//         const collectionItem = db.Collected.build({
-
-//         })
-//     }))
 
 
 
